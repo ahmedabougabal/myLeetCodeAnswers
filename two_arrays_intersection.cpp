905f9a08e910eaec314a9cd013b92d1fe0e69c41
ahmedabougabal <ahmedabougabal@live.com>
2024-07-03 08:20:24 +0300

    two_arrays_intersection

---
+++ two_arrays_intersection.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+  vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+  {
+    vector<int> result;
+    sort(nums1.begin(), nums1.end());
+    for (int i = 0; i < nums1.size(); i++)
+    {
+      for (int j = 0; j < nums2.size(); j++)
+      {
+        if (nums1[i] == nums2[j])
+        {
+          result.push_back(nums2[j]);
+          nums2.erase(nums2.begin() + j);
+          break;
+        }
+      }
+    }
+    return result;
+  }
+};
