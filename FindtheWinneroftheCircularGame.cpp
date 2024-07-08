3822abc61b550eacd723a22fb33efabc1503cee4
ahmedabougabal <ahmedabougabal@live.com>
2024-07-08 22:49:24 +0300

    "Josephus problem"

---
+++ FindtheWinneroftheCircularGame.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+// #include <queue>
+using namespace std;
+
+class Solution
+{
+public:
+  int findTheWinner(int n, int k)
+  {
+    int winner;
+    for (int i = 1; i <= n; i++)
+    {
+      winner = (winner + k) % i;
+    }
+    return winner;
+  }
+};
+
+int main()
+{
+  return 0;
+}
