c322315c53967cb7eac18611337d488671cac81d
ahmedabougabal <ahmedabougabal@live.com>
2024-07-27 19:28:35 +0300

    feat: implement expect function for testing with comments

--- toBeOrNotToBe.js
+++ toBeOrNotToBe.js
@@ -27,3 +27,52 @@ var expect = function (val) {
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
  */
+
+
+/** ways on how to solve this question
+ * 
+ * How to create an Object
+method 1
+
+let object = {
+    method1: function(arg1) {
+        // some code
+    },
+    method2: function(arg2) {
+        // some code
+    }
+};
+return object
+
+-------------method 2
+
+let object = new Object();
+object.method1 = function(arg) {
+      // some code
+}
+object.method2 = function(arg) {
+      // some code
+}
+return object
+--------------------------------
+How to create a function
+method 1
+
+functionNameOne: function(arg) => {
+    // do something.. return something..
+}
+    -----------------------------------
+method 2
+
+functionNameOne(arg) {
+    // do something.. return something..
+}
+------------------------------------
+method 3
+
+functionNameOne: (arg) => returnValue
+How to throw an error
+throw new Error("message");
+ * 
+ * 
+ */
