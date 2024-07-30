f5fec5271b149cb6dd130e3a186f8b08d56242aa
ahmedabougabal <ahmedabougabal@live.com>
2024-07-30 07:57:32 +0300

    Fixing the implementation of the brute force attempt [works only for small data sets]

--- countNumberOfTeams.cpp
+++ countNumberOfTeams.cpp
@@ -50,28 +50,28 @@ int main()
 ///////////////////////////////////////////////
 // //! Time limit Exceeded :'(
 // //! time complexity : O(n cube)
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-// class Solution
-// {
-// public:
-//   int numTeams(vector<int> &rating)
-//   {
-//     int count = 0;
-//     for (int i = 0; i < rating.size(); i++)
-//     {
-//       for (int j = 0; j < rating.size(); j++)
-//       {
-//         for (int k = 0; k < rating.size(); k++)
-//         {
-//           if (i != j && (rating[i] < rating[j] && rating[j] < rating[k]) || ((rating[i] > rating[j]) && (rating[j] > rating[k])))
-//           {
-//             count++;
-//           }
-//         }
-//       }
-//     }
-//     return count;
-//   }
-// };
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+  int numTeams(vector<int> &rating)
+  {
+    int count = 0;
+    for (int i = 0; i < rating.size(); i++)
+    {
+      for (int j = i + 1; j < rating.size(); j++)
+      {
+        for (int k = j + 1; k < rating.size(); k++)
+        {
+          if (i != j && (rating[i] < rating[j] && rating[j] < rating[k]) || ((rating[i] > rating[j]) && (rating[j] > rating[k])))
+          {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  }
+};
