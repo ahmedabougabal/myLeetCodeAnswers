900ff5d967ca4658a383ecc0818376eac8a96b82
ahmedabougabal <ahmedabougabal@live.com>
2024-08-24 23:59:08 +0300

    feat: used filter, map , reduce and recursion to remove all falsy values from either an object or arrays that hold falsy keys

---
+++ compactObject.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+  if(obj == null || obj == undefined || typeof(obj)=='object'){
+    return obj;
+  }
+  if(Array.isArray(obj)){
+    return obj.filter(Boolean).map(compactObject);
+  }
+  return Object.entries(obj).reduce((acc, [key,value])=>{
+    if(value){
+      acc[key] = compactObject(value)
+    }
+  },{})
+};
