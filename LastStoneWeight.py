bf42b4a5d63625c7b52681efebbcba1f87b7d616
ahmedabougabal <ahmedabougabal@live.com>
2024-11-02 20:17:18 +0200

    Feat: Implements the minHeap algo using the PriorityQueue return the last stone or return 0 if the queue is empty while smashing the 2 largest stones and then adding the subtraction value to the queue as the remaining of the 2 smashed stones use negation to reverse the minHeap to MaxHeap similar to the priority queue which is the default in C++

---
+++ LastStoneWeight.py
@@ -0,0 +1,16 @@
+from queue import PriorityQueue
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        pq = PriorityQueue()
+        for stone in stones:
+            pq.put(-stone)  # negate to act as a max heap since it is a minHeap
+
+        while pq.qsize() >= 2:
+            stone1 = -pq.get()
+            stone2 = -pq.get()
+            if stone1 > stone2:
+                pq.put(-(stone1 - stone2))
+
+        return -pq.get() if not pq.empty() else 0
