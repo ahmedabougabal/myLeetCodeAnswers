9e9367298e936b6090de2b644382c3aff1c66e8e
ahmedabougabal <ahmedabougabal@live.com>
2024-11-02 21:28:15 +0200

    Feat: Adds another appraoch using sorted list (no heap) and implementing the solution using the list methods sort(), append(), pop(), len()

--- LastStoneWeight.py
+++ LastStoneWeight.py
@@ -1,6 +1,5 @@
 from queue import PriorityQueue
 
-
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         pq = PriorityQueue()
@@ -14,3 +13,24 @@ class Solution:
                 pq.put(-(stone1 - stone2))
 
         return -pq.get() if not pq.empty() else 0
+
+
+"""
+another approach using sort
+"""
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones.sort()
+        while(len(stones)>1):
+            stone1 = stones.pop()
+            stone2 = stones.pop()
+            if stone1 > stone2:
+                stones.append(stone1-stone2)
+                stones.sort()
+        return stones[0] if stones else 0
+
+
+
+
+
