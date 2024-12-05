f2569099de82d7da141a1b51904dad93da2f069a
ahmedabougabal <ahmedabougabal@live.com>
2024-12-06 00:31:49 +0200

    feat(algo): Implements logic to calculate minimum operations required to sort a given array we start from the second number as operations done on the last element is not optimal starting from the second number , we compare it to the next number (if is less than or equal) if it is less than or equal to the next number we continue looping if not , we do ceiling division to get the number of parts needed to split the big number to split the number into x parts , we need x-1 operations to do so 9 -> 3,3,3 ? we need to convert 9 to 6 , 3 and 6 to 3,3 totalling 2 operations update the number 9 to the least minimum splitted number of itself (3) , 9 is 3+3+3, so we update 9 to 3 return the operations

--- MinimumReplacementstoSorttheArray.cpp
+++ MinimumReplacementstoSorttheArray.cpp
@@ -1,45 +1,25 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-/**
-, from my base of understanding , non decreasing means arranging the numbers in ascending order WHILE HAVING DUPLICATES
-example [10 , 5  ,13 ] => not sorted.
-we have to split 10 to numbers that add up and equal the next number.
 
-5 , 5 , 5 , 13 --> this is non decreasing order but can't be ascending cuz it have duplicates, the question needs to find the minimum number of times we need to split numbers to sort the array?
-so since we splitted only 10 to 5 and 5 then we return 1 , as 1
-was the min num of times we have to split a number to keep the array sorted.
-*/
 class Solution
 {
 public:
   long long minimumReplacement(vector<int> &nums)
   {
-    long long res = 0;
+    long long operations = 0; // return the number of min operations required
     int n = nums.size();
-    int max_allowed_value = nums[n - 1]; // starting from the last element
+    // leetcode hint 1  : not optimal to do operations on the last element so that is why we started from the second last element
     for (int i = n - 2; i >= 0; --i)
     {
-      int k;
-      if (nums[i] <= res)
+      //* nums[i+1] is the next number
+      if (nums[i] > nums[i + 1])
       {
-        max_allowed_value = nums[i];
-        continue;
-      }
-      else
-      {
-        k = (nums[0] + max_allowed_value - 1); // 10 +  mx(5) -1 // mx (5) = 2
-        res = nums[0] / k;
+        int parts = (nums[i] + nums[i + 1] - 1) / nums[i + 1]; //! ceiling division
+        operations += parts - 1;
+        nums[i] /= parts;
       }
     }
-    return res;
+    return operations;
   }
 };
-
-int main()
-{
-  vector<int> v = {10, 5, 13};
-  Solution sol;
-  cout << sol.minimumReplacement(v);
-  return 0;
-}
