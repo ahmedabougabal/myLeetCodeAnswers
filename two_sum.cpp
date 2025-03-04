258dff28e08e91c47d52ad58aea20e37252216c6
ahmedabougabal <ahmedabougabal@live.com>
2025-03-04 15:05:58 +0200

    feat: implements brute force solution to two_sum

---
+++ two_sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// solution 1
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            for (int j = i + 1; j < nums.size(); ++j)
+                if (nums[i] + nums[j] == target)
+                {
+                    result.push_back(i);
+                    result.push_back(j);
+                }
+        }
+        return result;
+    }
+};
