cbb4675d9dcc6ab6af4e7fb3afb4cb27a1c89890
ahmedabougabal <gabal@hamad.center>
2025-05-03 22:17:12 +0300

    feat: implements min heap (priority queue) to min heapify the queue when it finds a larger element than the smallest one in the heap and maintain the size of the heap to be k and return the kth largest element which is the top of the queue, time complexity is O(n log k) , n => for traversing through the nums vec and log k for maintaining the largest 2 kth elements (optimization)

--- KthLargestElementinanArray.cpp
+++ KthLargestElementinanArray.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
+//! using full vector sorting (works but needs optimization)
+// class Solution
+// {
+// public:
+//     int findKthLargest(vector<int> &nums, int k)
+//     {
+//         sort(nums.begin(), nums.end()); // time complexity is O(n*logn)
+//         int vector_size = nums.size();
+//         int index = vector_size - k;
+//         return nums[index];
+//     }
+// };
+
+// optimization from O(n log n) to O (n log k) using priority queue
 class Solution
 {
 public:
     int findKthLargest(vector<int> &nums, int k)
     {
-        sort(nums.begin(), nums.end());
-        int vector_size = nums.size();
-        int index = vector_size - k;
-        return nums[index];
+        // k will be the size of the priority queue (min heap) where the top of this queue will be the result we seek
+        priority_queue<int, vector<int>, greater<int>> q;
+        // set the size of min heap to k
+        for (auto i : nums)
+        {
+            // check if the 2 nodes inside the min heap has the top largest elements from the nums vector
+            q.push(i);
+            if (q.size() > k)
+            {
+                q.pop();
+            }
+        }
+        return q.top();
     }
 };
