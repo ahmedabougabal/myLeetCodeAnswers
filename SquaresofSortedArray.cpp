b270b2ea7d01ed1b88529e3794bcfd8a4970b8c8
ahmedabougabal <gabal@hamad.center>
2025-05-07 18:23:42 +0300

    feat(2-pointers): implements an optimized solution to solve the problem using 2 pointers in time : O(nlogn) and space O(n)

--- SquaresofSortedArray.cpp
+++ SquaresofSortedArray.cpp
@@ -1,6 +1,25 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// class Solution
+// {
+// public:
+//     vector<int> sortedSquares(vector<int> &nums)
+//     {
+//         vector<int> result;
+//         for (int i = 0; i < nums.size(); ++i)
+//         {
+//             result.push_back(nums[i] * nums[i]);
+//         }
+//         sort(result.begin(), result.end());
+//         return result;
+//     }
+// };
+
+//* optimized version time : O(n log n) , space : O(n) or O (log n) , no built in sort (2 pointers)
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution
@@ -8,12 +27,25 @@ class Solution
 public:
     vector<int> sortedSquares(vector<int> &nums)
     {
-        vector<int> result;
-        for (int i = 0; i < nums.size(); ++i)
+        int ptr1 = 0;
+        int ptr2 = nums.size() - 1;
+        int idx_to_place_elem = nums.size() - 1;
+        vector<int> result(nums.size());
+        while (ptr1 <= ptr2)
         {
-            result.push_back(nums[i] * nums[i]);
+            if (abs(nums[ptr2]) > abs(nums[ptr1]))
+            {
+                result[idx_to_place_elem] = nums[ptr2] * nums[ptr2];
+                ptr2--;
+                idx_to_place_elem--;
+            }
+            else
+            {
+                result[idx_to_place_elem] = nums[ptr1] * nums[ptr1];
+                ptr1++;
+                idx_to_place_elem--;
+            }
         }
-        sort(result.begin(), result.end());
         return result;
     }
 };
