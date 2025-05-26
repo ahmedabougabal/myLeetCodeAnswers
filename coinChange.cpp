4484ddc8547a036b82408cc71a00298b04dd1f6c
ahmedabougabal <gabal@hamad.center>
2025-05-26 14:07:17 +0300

    feat: implements dynamic programming to find the fewest/min num of coins required to reach the target, used decision tree to break the main problem into sub problems to identify the pattern of the main task using the bottom up approach (tabulaization)

---
+++ coinChange.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int coinChange(vector<int> &coins, int amount)
+    {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i)
+        {
+            for (int j = 0; j < coins.size(); ++j)
+            {
+                if (coins[j] <= i)
+                {
+                    if (dp[i - coins[j]] != INT_MAX)
+                    {
+                        dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+                    }
+                }
+            }
+        }
+        if (dp[amount] == INT_MAX)
+        {
+            return -1;
+        }
+        return dp[amount];
+    }
+};
