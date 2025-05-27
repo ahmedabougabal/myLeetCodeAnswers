c125c68f3f277ca4f77a2b83c5e4cfde572cf9d8
ahmedabougabal <gabal@hamad.center>
2025-05-27 19:55:07 +0300

    deprature: adds question starter / checkpoint

---
+++ PascalTriangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> result;
+        return result;
+    }
+};
