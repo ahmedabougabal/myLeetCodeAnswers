538ac1ac8072b6c6846f3b50e1386d597ec5f703
ahmedabougabal <gabal@hamad.center>
2025-05-29 19:47:00 +0300

    feat(dp): implements bottom-up tabulaization to add newRow vector to dp 'vector of vectors' and forming the pascal sequence

--- PascalTriangle.cpp
+++ PascalTriangle.cpp
@@ -7,7 +7,28 @@ class Solution
 public:
     vector<vector<int>> generate(int numRows)
     {
-        vector<vector<int>> result;
-        return result;
+        vector<vector<int>> dp;
+        if (numRows >= 1)
+        {
+            dp.push_back({1});
+        }
+        if (numRows >= 2)
+        {
+            dp.push_back({1, 1});
+        }
+        for (int i = 2; i < numRows; ++i)
+        {
+            vector<int> newRow;
+            newRow.push_back(1);
+
+            for (int j = 0; j < i - 1; ++j)
+            {
+                newRow.push_back(dp[i - 1][j] + dp[i - 1][j + 1]);
+            }
+
+            newRow.push_back(1);
+            dp.push_back(newRow);
+        }
+        return dp;
     }
 };
