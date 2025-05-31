0156539be99c716b645746c6cba3cc6fa5a103b3
ahmedabougabal <gabal@hamad.center>
2025-05-31 21:18:47 +0300

    feat(dp): implements dynamic programming to solve the triangular sum array reduction logic using tabulization as dp which is the vector of vectors is the table

--- FindTriangularSumofanArray.cpp
+++ FindTriangularSumofanArray.cpp
@@ -7,17 +7,24 @@ class Solution
 public:
     int triangularSum(vector<int> &nums)
     {
+        int result = 0;
+        vector<vector<int>> dp;
         if (nums.size() == 1)
-            return nums[0];
-
-        vector<int> newNums(nums.size() - 1);
-        int i = 0;
-        while (i < nums.size() - 1)
         {
-            newNums[i] = (nums[i] + nums[i + 1]) % 10;
-            i++;
+            result = nums[0];
+            return result;
         }
-        nums = newNums;
-        return triangularSum(nums);
+        dp.push_back(nums);
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            vector<int> newRow;
+            for (int j = 0; j < dp[i].size() - 1; ++j)
+            {
+                newRow.push_back((dp[i][j] + dp[i][j + 1]) % 10);
+            }
+            dp.push_back(newRow);
+        }
+        return dp[nums.size() - 1][0];
     }
 };
