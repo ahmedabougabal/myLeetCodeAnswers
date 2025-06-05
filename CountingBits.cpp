96c7b62005797014f0b2fb83348f9039f97865e2
ahmedabougabal <gabal@hamad.center>
2025-06-05 15:36:41 +0300

    feat: implements brute force solution for counting setbits of the integer number in binary and pushing its setbits to a vector in O(n log n)

--- CountingBits.cpp
+++ CountingBits.cpp
@@ -1,18 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 class Solution
 {
 public:
+    string decimalToBinary(int n)
+    {
+        if (n == 0)
+        {
+            return "0";
+        }
+        if (n == 1)
+        {
+            return "1";
+        }
+        return decimalToBinary(n / 2) + (n % 2 == 0 ? "0" : "1");
+    }
+
     vector<int> countBits(int n)
     {
         vector<int> result;
-        result.push_back(0);
         for (int i = 0; i <= n; ++i)
         {
-            result.push_back(__builtin_popcount(n));
+            string binary = decimalToBinary(i);
+            int oneCount = 0;
+            for (char c : binary)
+            {
+                if (c == '1')
+                {
+                    oneCount++;
+                }
+            }
+            result.push_back(oneCount);
         }
         return result;
     }
 };
+
+int main()
+{
+    Solution sol;
+    // test case 1
+    int input = 2;
+    vector<int> result = sol.countBits(2);
+    for (int k : result)
+    {
+        cout << k << " ";
+    }
+    return 0;
+}
