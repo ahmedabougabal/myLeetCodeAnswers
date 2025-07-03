173227dd8c1a18ad208a18f77a3a1de8ad9fedc6
ahmedabougabal <gabal@hamad.center>
2025-07-03 23:43:51 +0300

    feat: adds binary search algo trying to optimize the brute force approach due to TLE

---
+++ SearchInsertPosition.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int binarySearch(int low, int high, vector<int> vec, int target)
+    {
+        int mid = low + (high - low) / 2;
+        while (low <= high)
+        {
+            if (target == vec[mid])
+            {
+                return mid;
+            }
+            else if (target > vec[mid])
+
+                low = mid + 1;
+
+            else
+
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int result;
+        return binarySearch(0, nums.size() - 1, nums, target);
+
+        // for (int i = 0; i < nums.size(); ++i)
+        // {
+        //     if (nums[i] == target)
+        //     {
+        //         result = i;
+        //         break;
+        //     }
+        //     else
+        //     {
+        //         nums.push_back(target);
+        //         sort(nums.begin(), nums.end());
+        //         // auto it = lower_bound(nums.begin(), nums.end(), target);
+        //         for (int i = 0; i < nums.size(); ++i)
+        //         {
+        //             if (nums[i] == target)
+        //             {
+        //                 result = i;
+        //             }
+        //         }
+        //     }
+        // }
+        return result;
+    }
+};
