697a3ed159669500fd0c2b79e992c514ceafd99f
ahmedabougabal <gabal@hamad.center>
2025-07-07 10:26:32 +0300

    feat: implements the full solution of returning the longest common prefix if a mismatch is found, i return result immediately adds character to result in the outer loop adds a helper function to return the min size of a string in the vector

--- longestCommonPrefix.cpp
+++ longestCommonPrefix.cpp
@@ -5,22 +5,40 @@ using namespace std;
 class Solution
 {
 public:
+    // find the lenght of the shortest string in strs
+    int shortestString(vector<string> vec)
+    {
+        int stringWithLeastSize = vec[0].size();
+        if (vec.empty())
+        {
+            return 0;
+        }
+        for (auto i = 1; i < vec.size(); ++i)
+        {
+            if (vec[i].size() < stringWithLeastSize)
+            {
+                stringWithLeastSize = vec[i].size();
+            }
+        }
+        return stringWithLeastSize;
+    }
     string longestCommonPrefix(vector<string> &strs)
     {
         auto vector_size = strs.size();
         string result = "";
-        // find the lenght of the shortest string in strs
-        int min_lenght_of_strings;
-        for (int i = 0; i < min_lenght_of_strings; ++i)
+
+        int stringWithLeastSize = shortestString(strs);
+        for (int i = 0; i < stringWithLeastSize; ++i)
         {
             char c = strs[0][i];
             for (int j = 1; j < strs.size(); ++j)
             {
                 if (strs[j][i] != c)
                 {
-                    result += c;
+                    return result;
                 }
             }
+            result += c;
         }
         return result;
     }
