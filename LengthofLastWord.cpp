ed7ef204f1d7c638621fcd51cbe93960efb7a9cf
ahmedabougabal <gabal@hamad.center>
2025-07-19 17:56:13 +0300

    feat: implements a simpler approach where the first while loop skips trailing spaces , and the second while counts word characters

--- LengthofLastWord.cpp
+++ LengthofLastWord.cpp
@@ -44,3 +44,26 @@ public:
         return end - start;
     }
 };
+
+// a more simpler approach
+
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int result = 0;
+        int i = s.size() - 1;
+        while (i >= 0 && s[i] == ' ')
+        {
+            i--;
+        }
+
+        while (i >= 0 && s[i] != ' ')
+        {
+            result++;
+            i--;
+        }
+        return result;
+    }
+};
