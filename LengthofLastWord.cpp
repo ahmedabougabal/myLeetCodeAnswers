3738c87707184246f09df2eb9248634b8f26b141
ahmedabougabal <gabal@hamad.center>
2025-07-19 16:38:07 +0300

    feat: adds logic for calculating the length of the last word in the string after performing a trail whitespaces trim on the original string using a helper function

---
+++ LengthofLastWord.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    std::string trim(const std::string &str,
+                     const std::string &whitespace = " \t")
+    {
+        const auto strBegin = str.find_first_not_of(whitespace);
+        if (strBegin == std::string::npos)
+            return ""; // no content
+
+        const auto strEnd = str.find_last_not_of(whitespace);
+        const auto strRange = strEnd - strBegin + 1;
+
+        return str.substr(strBegin, strRange);
+    }
+    int lengthOfLastWord(string s)
+    {
+        string trimmedString = trim(s, " \t");
+        int size = trimmedString.size() - 1;
+        int result = 0;
+
+        for (int i = size; i >= 0; --i)
+        {
+            if (trimmedString[i] == ' ')
+            {
+                break;
+            }
+            result++;
+        }
+        return result;
+    }
+};
+
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        const auto end = s.find_last_not_of(' ');
+        const auto start = s.find_last_of(' ', end);
+        return end - start;
+    }
+};
