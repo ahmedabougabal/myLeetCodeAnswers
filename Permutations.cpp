378abf2582b02d2a855ee06eb89e3d705e14a5b2
ahmedabougabal <gabal@hamad.center>
2025-07-21 12:22:40 +0300

    feat: implement recursive backtracking solution for array permutations
    add dps() helper function using divide-and-conquer approach
    fix elements from left to right with backtracking to explore all possibilities

---
+++ Permutations.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void dps(vector<int> &nums, int l, int r, vector<vector<int>> &result)
+    {
+        r = nums.size() - 1;
+        if (l == r)
+        {
+            result.push_back(nums);
+            return;
+        }
+        for (int i = l; i <= r; ++i)
+        {
+            int tmp = nums[l];
+            nums[l] = nums[i];
+            nums[i] = tmp;
+            dps(nums, l + 1, r, result);
+            int tmp2 = nums[l];
+            nums[l] = nums[i];
+            nums[i] = tmp2;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> result;
+        int firstIndex = 0;
+        int lastIndex = nums.size() - 1;
+        dps(nums, firstIndex, lastIndex, result);
+        return result;
+    }
+};
