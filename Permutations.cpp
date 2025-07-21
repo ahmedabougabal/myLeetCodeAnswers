7a696324b2a01dc266605e773021830adfc579c6
ahmedabougabal <gabal@hamad.center>
2025-07-21 19:43:01 +0300

    feat: sorts array, adds do while logic to push nums to the container and then push the next permuation based on the std::next_permuatation (the next greatest lexicographical permuation)

--- Permutations.cpp
+++ Permutations.cpp
@@ -33,3 +33,19 @@ public:
         return result;
     }
 };
+
+class Solution
+{
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+
+        do
+        {
+            result.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+        return result;
+    }
+};
