9173ef1c1e9e81c9c31d453183a52899af22ba38
ahmedabougabal <gabal@hamad.center>
2025-08-12 19:08:56 +0300

    ref(optimize): adds 3 if conditions within the 3 nested for loops to skip duplicates in the process of generating triplets into result 2d vector

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -6,27 +6,12 @@ using namespace std;
 class Solution
 {
 public:
-    int removeDuplicatesFromSortedVectors(vector<int> nums)
-    {
-        int unique = 0;
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size(); ++i)
-        {
-            if (nums[i] != nums[unique])
-            {
-                unique++;
-                nums[unique] = nums[i];
-            }
-        }
-        return unique + 1;
-    }
-
-    void removeTripletDuplicatesFromVectorOfVectors(vector<vector<int>> vec)
-    {
-        sort(vec.begin(), vec.end());
-        auto last = std::unique(vec.begin(), vec.end());
-        vec.erase(last, vec.end());
-    }
+    // void removeTripletDuplicatesFromVectorOfVectors(vector<vector<int>> &vec)
+    // {
+    //     sort(vec.begin(), vec.end());
+    //     auto last = std::unique(vec.begin(), vec.end());
+    //     vec.erase(last, vec.end());
+    // }
 
     //[nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
     vector<vector<int>> threeSum(vector<int> &nums)
@@ -37,10 +22,22 @@ public:
 
         for (int i = 0; i < nums.size(); ++i)
         {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
             for (int j = i + 1; j < nums.size(); ++j)
             {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                {
+                    continue;
+                }
                 for (int k = j + 1; k < nums.size(); ++k)
                 {
+                    if (k > j + 1 && nums[k] == nums[k - 1])
+                    {
+                        continue;
+                    }
 
                     if (nums[i] + nums[j] + nums[k] == 0)
                     {
@@ -50,7 +47,7 @@ public:
                 }
             }
         }
-        removeTripletDuplicatesFromVectorOfVectors(result);
+        // removeTripletDuplicatesFromVectorOfVectors(result);
         return result;
     }
 };
