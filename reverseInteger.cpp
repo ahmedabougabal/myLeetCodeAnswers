c62624f091783818c15afcfd0c6f0c75231418fe
ahmedabougabal <gabal@hamad.center>
2025-08-16 15:35:56 +0300

    feat: adds postive and negative integer handling for reversing an integer by converting it to a string and then reverse it using std::reverse() and string iterators then converting back to integer using stoi()

---
+++ reverseInteger.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        int result;
+        // integer to string
+        int negativeNum;
+        if (x < 0)
+        {
+            long long newX = static_cast<long long>(x);
+            newX *= -1;
+            string convertNegativeNumToString = to_string(newX);
+            std::reverse(convertNegativeNumToString.begin(), convertNegativeNumToString.end());
+            negativeNum = stoi(convertNegativeNumToString);
+            return negativeNum * -1;
+        }
+        else
+        {
+            string convertNumToString = to_string(x);
+            std::reverse(convertNumToString.begin(), convertNumToString.end());
+            result = stoi(convertNumToString);
+        }
+        return result;
+    }
+};
