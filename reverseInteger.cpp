bd9e2af308096e053614a754a98234574ba481a8
Ahmed Abou Gabal <ahmedabougabal@live.com>
2025-08-16 16:28:45 +0300

    feat: readds the static casting of the int x and multiplies it by -1 and adds an overflow check in the 2 if conditions

--- reverseInteger.cpp
+++ reverseInteger.cpp
@@ -5,27 +5,25 @@
 #include <limits.h>
 using namespace std;
 
-// todo
-//  account for integer overflow when dealing with huge numbers as 2^31 - 1
-// need to refactor this static cast logic
 class Solution
 {
 public:
     int reverse(int x)
     {
         long long result;
-        if (x >= INT_MAX || x <= INT_MIN)
-        {
-            return 0;
-        }
         // integer to string
         long long negativeNum;
         if (x < 0)
         {
-            x *= -1;
-            string convertNegativeNumToString = to_string(x);
+            long long newX = static_cast<long long>(x * 1);
+            newX *= -1;
+            string convertNegativeNumToString = to_string(newX);
             std::reverse(convertNegativeNumToString.begin(), convertNegativeNumToString.end());
             negativeNum = stoll(convertNegativeNumToString) * -1;
+            if (negativeNum < INT_MIN)
+            {
+                return 0;
+            }
             return negativeNum;
         }
         else
@@ -33,6 +31,10 @@ public:
             string convertNumToString = to_string(x);
             std::reverse(convertNumToString.begin(), convertNumToString.end());
             result = stoll(convertNumToString);
+            if (result > INT_MAX)
+            {
+                return 0;
+            }
         }
         return result;
     }
