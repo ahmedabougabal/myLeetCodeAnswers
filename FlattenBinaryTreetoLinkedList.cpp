eb37564b57c1691c97995b8c4e274f9ddebb168a
ahmedabougabal <ahmedabougabal@live.com>
2025-11-01 21:04:08 +0200

    feat: adds an optimized algorithm to flatten the binary tree to a right skewed tree in place finds rightmost node in the left subtree of the root by flatteninng both root's left and right subtrees connects that rightmost node (last node) to the right subtree move root's right pointer to first node of the left subtree and set its left pointer to null to form that right skewed shape

--- FlattenBinaryTreetoLinkedList.cpp
+++ FlattenBinaryTreetoLinkedList.cpp
@@ -13,28 +13,66 @@ struct TreeNode
   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// class Solution
+// {
+// public:
+//   void preOrderTraversal(TreeNode *root, vector<int> &treeNodes)
+//   {
+//     if (root)
+//     {
+//       treeNodes.push_back(root->val);
+//       preOrderTraversal(root->left, treeNodes);
+//       preOrderTraversal(root->right, treeNodes);
+//     }
+//   }
+//   void flatten(TreeNode *root)
+//   {
+//     vector<int> vec;
+//     preOrderTraversal(root, vec);
+//     TreeNode *linkedListNode = root;
+//     for (int i = 1; i < vec.size(); ++i)
+//     {
+//       linkedListNode->left = nullptr;
+//       linkedListNode->right = new TreeNode(vec[i]);
+//       linkedListNode = linkedListNode->right;
+//     }
+//   }
+// };
+
+// inplace insertion
+/**
+ *
+Flatten the left child of root
+Flatten the right child of root
+Set the right child of the last node in root.left to root.right
+Set root.right to root.left
+Set root.left to NULLs
+ */
 class Solution
 {
 public:
-  void preOrderTraversal(TreeNode *root, vector<int> &treeNodes)
-  {
-    if (root)
-    {
-      treeNodes.push_back(root->val);
-      preOrderTraversal(root->left, treeNodes);
-      preOrderTraversal(root->right, treeNodes);
-    }
-  }
   void flatten(TreeNode *root)
   {
-    vector<int> vec;
-    preOrderTraversal(root, vec);
-    TreeNode *linkedListNode = root;
-    for (int i = 1; i < vec.size(); ++i)
+    if (!root)
     {
-      linkedListNode->left = nullptr;
-      linkedListNode->right = new TreeNode(vec[i]);
-      linkedListNode = linkedListNode->right;
+      return;
+    }
+    flatten(root->left);
+    flatten(root->right);
+
+    if (root->left)
+    {
+      TreeNode *lastNode = root->left;
+      while (lastNode->right)
+      {
+        lastNode = lastNode->right;
+      }
+
+      lastNode->right = root->right;
+
+      root->right = root->left;
+
+      root->left = nullptr;
     }
   }
 };
